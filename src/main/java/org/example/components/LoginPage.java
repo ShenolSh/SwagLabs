49432bcd5375f291e7afb883d444725be00395df
ShenolShaban <s.shaban@7mojos.com>
2025-01-22 15:21:26 +0200

    changes

--- src/main/java/org/example/components/LoginPage.java
+++ src/main/java/org/example/components/LoginPage.java
@@ -3,4 +3,5 @@ package org.example.components;
 import org.example.core.BaseActions;
 
 public class LoginPage extends BaseActions {
+        private static final String loginPageURL = "https://www.saucedemo.com/";
 }
