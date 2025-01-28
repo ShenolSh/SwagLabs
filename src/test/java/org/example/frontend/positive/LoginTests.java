59f02d29f9c2d20cc5cc3908b6e99e372f2444b9
ShenolSh <shenol.suphi.shaban@gmail.com>
2025-01-28 10:13:27 +0200

    add first positive test for LoginPage and add ProductsPage as well

--- src/test/java/org/example/frontend/positive/LoginTests.java
+++ src/test/java/org/example/frontend/positive/LoginTests.java
@@ -1,6 +1,18 @@
 package org.example.frontend.positive;
 
+import org.example.components.LoginPage;
+import org.example.components.ProductsPage;
 import org.example.core.BaseTest;
+import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTest {
+
+    @Test
+    public void successful_login_test_standard_user(){
+        LoginPage.open();
+        LoginPage.Login("standard_user", "secret_sauce" );
+        ProductsPage.verifySuccessfulLoginStandardUser("Products", "The logging in test with standard_user account was not successful");
+    }
+
+
 }
