d60d6348ca8ce1068b1755ebade5c97478999338
ShenolSh <shenol.suphi.shaban@gmail.com>
2025-02-11 21:49:29 +0200

    Sorting test by price low to high

--- src/main/java/org/example/components/ProductsPage.java
+++ src/main/java/org/example/components/ProductsPage.java
@@ -13,24 +13,21 @@ import java.util.List;
 public class ProductsPage extends BaseActions {
     private static final By PRODUCTS = By.cssSelector(".header_secondary_container .title");
     private static final By PRODUCT_SORT_CONTAINER = By.cssSelector(".product_sort_container");
-
     private static final By PRODUCTS_TITLES = By.cssSelector(".inventory_item_name");
+    private static final By PRODUCTS_PRICE = By.cssSelector(".inventory_item_price");
 
     public static void verifySuccessfulLoginStandardUser(String expectedProductsContainerOnSuccessfulLogin, String messageOnFailedLogin) {
         String actualResultAfterLoggingIn = waitForElementVisibility(PRODUCTS, 10).getText();
         Assert.assertEquals(actualResultAfterLoggingIn, expectedProductsContainerOnSuccessfulLogin, messageOnFailedLogin);
     }
-
     public static void SortAToZ() {
         Select productSortContainer = new Select(Browser.driver.findElement(PRODUCT_SORT_CONTAINER));
         productSortContainer.selectByValue("az");
     }
-
     public static void SortZToA() {
         Select productSortContainer = new Select(Browser.driver.findElement(PRODUCT_SORT_CONTAINER));
         productSortContainer.selectByValue("za");
     }
-
     public static void verifySuccessfulSortAToZ(List<String> expectedProductsAlphabeticalOrderAToZ, String messageOnWrongSortingResult) {
         List<WebElement> itemTitles = Browser.driver.findElements(PRODUCTS_TITLES);
         ArrayList<String> actualProductsTitlesSortedAToZ = new ArrayList<>();
@@ -40,7 +37,17 @@ public class ProductsPage extends BaseActions {
         Assert.assertEquals(actualProductsTitlesSortedAToZ, expectedProductsAlphabeticalOrderAToZ, messageOnWrongSortingResult);
 
     }
-
-
+    public static void SortPriceLowToHigh() {
+        Select productSortContainer = new Select(Browser.driver.findElement(PRODUCT_SORT_CONTAINER));
+        productSortContainer.selectByValue("lohi");
+    }
+    public static void verifySuccessfulSortPriceLowToHigh(List<String> expectedProductsPriceLowToHigh, String messageOnWrongSortingLowToHighResult) {
+        List<WebElement> itemTitles = Browser.driver.findElements(PRODUCTS_PRICE);
+        ArrayList<String> actualProductsPriceSortedLowToHigh = new ArrayList<>();
+        for (WebElement itemTitle : itemTitles) {
+            actualProductsPriceSortedLowToHigh.add(itemTitle.getText());
+        }
+        Assert.assertEquals(actualProductsPriceSortedLowToHigh, expectedProductsPriceLowToHigh, messageOnWrongSortingLowToHighResult);
+    }
 }
 
